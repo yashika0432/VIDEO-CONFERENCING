3b8e2c3216b82c9961a1d660e7c46cb2c22d3d1a
yashika <yashika0432@gmail.com>
2025-11-26 21:57:16 +0530

    ui for the webapp

---
+++ frontend/zoom/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./pages/landing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage></LandingPage>}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
