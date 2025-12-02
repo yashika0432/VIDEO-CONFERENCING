86c03abdd749268daa89a39a8b0e3bb3041a80f9
yashika <yashika0432@gmail.com>
2025-12-02 20:21:05 +0530

    creating models

---
+++ backend/src/models/meeting.model.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const meetingSchema = new Schema({
+  userId: { type: String },
+  meetingCode: { type: String, required: true },
+  date: { type: Date, default: Date.now, required: true },
+});
+
+const Meeting = mongoose.model(Meeting, meetingSchema);
+
+export { Meeting };
