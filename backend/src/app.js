fafd21a9af8a8fbc6115880dfd2d36eb74268152
yashika <yashika0432@gmail.com>
2025-12-02 20:13:53 +0530

    setting up the backend and db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,32 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import connectToSocket from "./controllers/socketManager.js";
+import cors from "cors";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.get("/home", (req, res) => {
-  return res.json({ hello: "world" });
-});
+app.set("port", process.env.PORT || 8000);
+
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 const start = async () => {
-  app.listen(8000, () => {
+  app.set("mongo_user", "yashika9654_db_user");
+  try {
+    const connectionDb = await mongoose.connect(
+      "mongodb+srv://yashika9654_db_user:yashika1234@zoom.rhnh9gq.mongodb.net/"
+    );
+    console.log(`mongo connected db host : ${connectionDb.connection.host}`);
+  } catch (err) {
+    console.log("Mongodb connection error", err);
+  }
+
+  server.listen(app.get("port"), () => {
     console.log("listening on port");
   });
 };
